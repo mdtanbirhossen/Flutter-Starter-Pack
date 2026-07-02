59eb436ffdb12cfe871528b9f9e9cfdb61acd4cd
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-07-02 22:35:42 +0600

    fix: fixed warnings

--- lib/core/constant/widgets/input_text_field.dart
+++ lib/core/constant/widgets/input_text_field.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../app_colors.dart';
 import '../app_text_styles.dart';
 
 class InputTextField extends StatelessWidget {
